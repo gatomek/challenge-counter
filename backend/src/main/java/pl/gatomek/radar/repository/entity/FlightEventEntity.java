7a5124410a3d5f22321059b5e75d2b2438598fa2
Gatomek <tomasz.galinski@gmail.com>
2025-11-17 01:43:08 +0100

    review

--- backend/src/main/java/pl/gatomek/radar/repository/entity/FlightEventEntity.java
+++ backend/src/main/java/pl/gatomek/radar/repository/entity/FlightEventEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -32,5 +33,5 @@ public class FlightEventEntity {
     private String comment;
 
     @Column(name = "timestamp")
-    private java.time.Instant timestamp;
+    private Instant timestamp;
 }
